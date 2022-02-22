c46a3b3c7ff912a7dd61e9736f44f8e8acb9ab2e
hashfunc <root@hashfunc.io>
2022-02-22 23:33:00 +0900

    feat: kv_handler

---
+++ src/handlers.rs
@@ -0,0 +1,17 @@
+use serde_json::json;
+use worker::*;
+
+pub async fn kv_handler(_req: Request, ctx: RouteContext<()>) -> Result<Response> {
+    let key = match ctx.param("key") {
+        None => { return Response::error("Bad request", 400); }
+        Some(key) => { key }
+    };
+    let response = ctx.kv("DATA")?
+        .get(key)
+        .text()
+        .await;
+    match response? {
+        None => { Response::error("Not found", 404) }
+        Some(value) => { Response::from_json(&json!(value)) }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 use worker::*;
 
+mod handlers;
+
 #[event(fetch)]
 pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Response> {
     Router::new()
-        .get("/", |_, _| Response::ok("Hello from Workers!"))
+        .get_async("/kv/:key", handlers::kv_handler)
         .run(req, env)
         .await
 }
